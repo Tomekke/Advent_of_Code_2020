243f3529235e5176319fa6c0838531ed5c795220
Thomas <thomas.standaert@kuleuven.vib.be>
2020-12-02 18:26:24 +0100

    Finished part 2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -3,6 +3,10 @@
 def check_password(min_number, max_number, letter, password):
     return True if min_number <= password.count(letter) <= max_number else False
 
+def check_password_2(min_number, max_number, letter, password):
+    # XOR
+    return True if (password[min_number] == letter) != (password[max_number] == letter) else False
+
 def main():
     filename = "inputfile"
     count = 0
@@ -11,7 +15,11 @@ def main():
             policy, password = line.split(':')
             numbers, letter = policy.split(' ')
             min_number, max_number = numbers.split('-')
-            if check_password(int(min_number), int(max_number), letter, password):
+            # Part 1
+            # if check_password(int(min_number), int(max_number), letter, password):
+            #     count += 1
+            # Part 2
+            if check_password_2(int(min_number), int(max_number), letter, password):
                 count += 1
     print(count)
 
