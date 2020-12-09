d8e37c4fa582f542676cc5f6651ee22ae7ec539e
Thomas <thomas.standaert@kuleuven.vib.be>
2020-12-09 10:09:52 +0100

    Working on Day8 part2

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -19,11 +19,38 @@ def check_instructions(lines):
             return accumulator
 
 
+def check_instructions_2(lines):
+    counter, accumulator = 0, 0
+    counters_used = []
+    while counter < len(lines):
+        if counter not in counters_used:
+            counters_used.append(counter)
+            instruction, amount = lines[counter].strip().split(' ')
+            if instruction == 'acc':
+                accumulator += int(amount)
+                counter += 1
+            elif instruction == 'jmp':
+                counter += int(amount)
+            elif instruction == 'nop':
+                counter += 1
+            else:
+                print('Unknown operation')
+        else:
+            return accumulator
+
+
 def main():
-    filename='inputfile'
+    filename='testfile'
     with open(filename, 'r') as fh:
         lines = fh.readlines()
-    print(check_instructions(lines))
+    """
+    Part 1
+    """
+    # print(check_instructions(lines))
+    """
+    Part 2
+    """
+    print(check_instructions_2(lines))
 
 
 if __name__ == '__main__':
